1b6f0e5907e60ca5b46e01519cfaa85036acf87c
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-18 11:17:36 +0530

    Parent File Created

---
+++ E-waller-application/src/main/java/com/example/E_waller_application/EWallerApplication.java
@@ -0,0 +1,13 @@
+package com.example.E_waller_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWallerApplication.class, args);
+	}
+
+}
---
+++ E-waller-application/src/test/java/com/example/E_waller_application/EWallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_waller_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
