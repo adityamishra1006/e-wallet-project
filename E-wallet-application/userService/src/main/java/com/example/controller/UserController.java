af7e10eb9f0a4d07f83db515bb52385464b99bbb
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-18 18:55:52 +0530

    DTO done and controller design start

--- E-wallet-application/userService/src/main/java/com/example/controller/UserController.java
+++ E-wallet-application/userService/src/main/java/com/example/controller/UserController.java
@@ -1,5 +1,18 @@
 package com.example.controller;
 
+import com.example.dto.UserDTO;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user-service")
 public class UserController {
 
+    @PostMapping("/user")
+    public Long createUser(@RequestBody @Valid UserDTO userDTO){
+
+    }
 }
