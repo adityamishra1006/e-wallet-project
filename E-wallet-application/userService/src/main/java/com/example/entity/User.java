9d01c0b24cedde09dc41d1c15141c0afca8952e1
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-18 18:48:54 +0530

    Configuring user-service

---
+++ E-wallet-application/userService/src/main/java/com/example/entity/User.java
@@ -0,0 +1,16 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Setter
+@Getter
+public class User {
+    
+}
