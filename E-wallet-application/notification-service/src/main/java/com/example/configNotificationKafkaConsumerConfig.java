b0164d04069b616c35a72a82ed71a2ad41f0ae66
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-19 13:04:00 +0530

    Notification Service
    Configuring Notification Service so that it can send email to the registered email id. It can generate notification in the form of email

--- E-wallet-application/notification-service/src/main/java/com/example/configNotificationKafkaConsumerConfig.java
+++ E-wallet-application/notification-service/src/main/java/com/example/configNotificationKafkaConsumerConfig.java
@@ -1,5 +1,27 @@
 package com.example;
 
+import com.example.dto.UserCreatedPayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import tools.jackson.databind.ObjectMapper;
+
+@Configuration
 public class configNotificationKafkaConsumerConfig {
-    
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(configNotificationKafkaConsumerConfig.class);
+    private static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    @KafkaListener(topics = "${user.created.topic}", groupId = "email")
+    public void consumerUserCreateTopic(String payload){
+        UserCreatedPayload userCreatedPayload = OBJECT_MAPPER.readValue(payload.value().toString(), UserCreatedPayload.class);
+        MDC.put("requestId", userCreatedPayload.getRequestId());
+        LOGGER.info("Read from kafka : {}", userCreatedPayload);
+
+        //Sending email 
+
+        MDC.clear();
+    }
 }
