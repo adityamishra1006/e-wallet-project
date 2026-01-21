737d255c666b00d5ca0be7a9dbbb17e20d58b366
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-21 13:06:44 +0530

    Created New Spring Project

---
+++ e-wallet-project/src/main/java/com/example/e_wallet_project/EWalletProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_wallet_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWalletProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWalletProjectApplication.class, args);
+	}
+
+}
---
+++ e-wallet-project/src/test/java/com/example/e_wallet_project/EWalletProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_wallet_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
