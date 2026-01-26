c683662ad8bedb9b7a19040dd8a30939a6a08a37
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-26 22:51:16 +0530

    Update TransactionStatusDTO
    Changed type of status to TxnStatus(enum)

--- e-wallet-project/transaction-service/src/main/java/com/example/dto/TransactionStatusDTO.java
+++ e-wallet-project/transaction-service/src/main/java/com/example/dto/TransactionStatusDTO.java
@@ -1,12 +1,13 @@
 package com.example.dto;
 
+import com.example.entity.TxnStatus;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class TransactionStatusDTO {
-    private String status;
+    private TxnStatus status;
     private String reason;
 }
 
