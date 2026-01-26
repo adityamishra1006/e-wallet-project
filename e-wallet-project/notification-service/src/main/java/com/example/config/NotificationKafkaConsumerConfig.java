0ed8f8162250e40ee12c52d482d4c0c4f7bcd2b7
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-26 15:38:00 +0530

    Build Kafka Consumer Configuration for Notification Service

--- e-wallet-project/notification-service/src/main/java/com/example/config/NotificationKafkaConsumerConfig.java
+++ e-wallet-project/notification-service/src/main/java/com/example/config/NotificationKafkaConsumerConfig.java
@@ -1,5 +1,24 @@
 package com.example.config;
 
+
+import com.example.dto.UserCreatedPayload;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.MDC;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import tools.jackson.databind.ObjectMapper;
+
+@Slf4j
+@Configuration
 public class NotificationKafkaConsumerConfig {
-    
+    private static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    @KafkaListener(topics = "${user.created.topic}", groupId = "email")
+    public void consumeUserCreatedTopic(ConsumerRecord payload){
+        UserCreatedPayload userCreatedPayload = OBJECT_MAPPER.readValue(payload.value().toString(), UserCreatedPayload.class);
+        MDC.put("requestId", userCreatedPayload.getRequestId());
+        log.info("Read from kafka : {}", userCreatedPayload);
+
+    }
 }
