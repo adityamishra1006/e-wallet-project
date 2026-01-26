7c9ad00e13114b4dcdc41c4d8bee272fe8764ae5
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-26 22:20:18 +0530

    Create TransactionRepo

--- e-wallet-project/transaction-service/src/main/java/com/example/repo/TransactionRepo.java
+++ e-wallet-project/transaction-service/src/main/java/com/example/repo/TransactionRepo.java
@@ -2,6 +2,8 @@ package com.example.repo;
 
 import com.example.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {
 }
