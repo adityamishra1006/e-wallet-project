7f24eae3ae93e4a338966f1f320d0a97fc56eeb8
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-26 22:51:34 +0530

    Created Controller for Transaction Service

--- e-wallet-project/transaction-service/src/main/java/com/example/controller/TransactionController.java
+++ e-wallet-project/transaction-service/src/main/java/com/example/controller/TransactionController.java
@@ -1,4 +1,35 @@
 package com.example.controller;
 
+import com.example.dto.TransactionRequestDTO;
+import com.example.dto.TransactionStatusDTO;
+import com.example.service.TransactionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping("/transaction-service")
+@Slf4j
+@RequiredArgsConstructor
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping("/txn")
+    public ResponseEntity<String> initTransaction(@RequestBody @Valid TransactionRequestDTO requestDTO) throws ExecutionException, InterruptedException {
+        log.info("Starting transaction : {}", requestDTO);
+
+        String txnId = transactionService.initTransaction(requestDTO);
+        return ResponseEntity.accepted().body(txnId);
+    }
+
+    @GetMapping("/status/{txnId}")
+    public ResponseEntity<TransactionStatusDTO> getTxnStatus(@PathVariable String txnId){
+        return ResponseEntity.ok(transactionService.getStatus(txnId));
+    }
+
 }
