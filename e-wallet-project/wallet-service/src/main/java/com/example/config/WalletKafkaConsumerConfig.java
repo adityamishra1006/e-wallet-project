fea4356f9402ed1b3326baf6e35232679bf82589
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-27 11:04:36 +0530

    Update WalletKafkaConsumerConfig
    Added a method of consumeTransactionInitializationTopic

--- e-wallet-project/wallet-service/src/main/java/com/example/config/WalletKafkaConsumerConfig.java
+++ e-wallet-project/wallet-service/src/main/java/com/example/config/WalletKafkaConsumerConfig.java
@@ -1,8 +1,12 @@
 package com.example.config;
 
+import com.example.dto.TxnInitPayload;
 import com.example.dto.UserCreatedPayload;
 import com.example.entity.Wallet;
 import com.example.repo.WalletRepo;
+import com.example.service.WalletService;
+import jakarta.transaction.Transaction;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.MDC;
@@ -11,16 +15,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
 import tools.jackson.databind.ObjectMapper;
 
+import java.util.concurrent.ExecutionException;
+
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class WalletKafkaConsumerConfig {
     private static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    @Autowired
-    private WalletRepo walletRepo;
-
-//    @Autowired
-//    private WalletService walletService;
+    private final WalletRepo walletRepo;
+    private final WalletService walletService;
 
     @KafkaListener(topics = "${user.created.topic}", groupId = "wallet")
     public void consumeUserCreatedTopic(ConsumerRecord payload){
@@ -34,6 +38,18 @@ public class WalletKafkaConsumerConfig {
         wallet.setUserEmail(userCreatedPayload.getUserEmail());
 
         walletRepo.save(wallet);
+
+        MDC.clear();
+    }
+
+    @KafkaListener(topics = "${txn.init.topic}", groupId = "wallet")
+    public void consumeTxnInitTopic(ConsumerRecord payload) throws ExecutionException, InterruptedException {
+        TxnInitPayload txnInitPayload = OBJECT_MAPPER.readValue(payload.value().toString(), TxnInitPayload.class);
+        MDC.put("requestId", txnInitPayload.getRequestId());
+        log.info("Read from kafka  : {}", txnInitPayload);
+
+        walletService.walletTxn(txnInitPayload);
+
 
         MDC.clear();
     }
