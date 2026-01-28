1f14cae3c3bf47932b09a1ba8001740b98ba6968
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-28 11:10:02 +0530

    Create WalletBalanceDTO

---
+++ e-wallet-project/common-code/src/main/java/com/example/dto/WalletBalanceDTO.java
@@ -0,0 +1,14 @@
+package com.example.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class WalletBalanceDTO {
+
+    private Double balance;
+
+}
