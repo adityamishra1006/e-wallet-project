dfee4da19845105f350c9cf41b78da3e49560b14
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-28 11:12:48 +0530

    Update UserController
    Developed an endpoint for getting profile details of the user.

--- e-wallet-project/user-service/src/main/java/com/example/controller/UserController.java
+++ e-wallet-project/user-service/src/main/java/com/example/controller/UserController.java
@@ -2,13 +2,12 @@ package com.example.controller;
 
 
 import com.example.dto.UserDTO;
+import com.example.dto.UserProfileDTO;
 import com.example.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ExecutionException;
 
@@ -22,5 +21,11 @@ public class UserController {
     @PostMapping("/user")
     public Long createUser(@RequestBody @Valid UserDTO userDTO) throws ExecutionException, InterruptedException {
         return userService.createUser(userDTO);
+    }
+
+    @GetMapping("/profile/{id}")
+    public ResponseEntity<UserProfileDTO> getProfile(@PathVariable Long id){
+        UserProfileDTO userProfileDTO = userService.getUserProfile(id);
+        return ResponseEntity.ok(userProfileDTO);
     }
 }
