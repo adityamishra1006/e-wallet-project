4c45be8bfd5d169f1a7abe62c09564a8d56b5068
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-28 09:59:30 +0530

    Update TxnCompletedPayload

--- e-wallet-project/common-code/src/main/java/com/example/dto/TxnCompletedPayload.java
+++ e-wallet-project/common-code/src/main/java/com/example/dto/TxnCompletedPayload.java
@@ -2,11 +2,13 @@ package com.example.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class TxnCompletedPayload {
-    private Long id; // Id of the transaction
+    private Long id; // I'd of the transaction
     private Boolean success;
     private String reason;
     private String requestId;
